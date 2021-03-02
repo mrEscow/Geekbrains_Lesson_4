43b720b9ba90719e0610b3b23c62381d44d95a36
mr.Escow <escowxbox@gmail.com>
2021-03-02 16:37:04 +0300

    написал первую программу

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
-#include "main.h"
+
+bool foo_1(float &a,float &b){
+
+    if(a + b <= 20 && a + b >= 10){
+        //std::cout << "true"  << std::endl;
+        return true;
+    }
+    else {
+        //std::cout << "false" << std::endl;
+        return false;
+    }
+}
 
 int main(int argc, char const *argv[]) {
+
+
+    /*
+    1.Ќаписать программу,
+    провер€ющую что сумма двух чисел
+    лежит в пределах от 10 до 20 (включительно),
+    если да Ц вывести true,
+    в противном случае Ц false;
+    */
+
+    float a, b;
+
+    a = 70;
+    b = 8;
+
+    std::cout
+    << foo_1(a,b)
+    <<
+    std::endl;
+
 
     return 0;
 }
